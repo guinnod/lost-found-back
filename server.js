56ed6c9590985352e5f45b4c91501004dc93837f
guinnod <210103064@stu.sdu.edu.kz>
2023-11-17 10:03:34 +0600

    vercel

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const app = require("./app");
 const env = require("dotenv");
 const connectDB = require("./config/db");
 
-env.config({path: "./config/.env"});
+env.config();
 
-const start = async() => {
+const start = async () => {
     try {
         await connectDB();
 
         app.listen(process.env.PORT, () => {
-            console.log(`Server is working on http://localhost:${process.env.PORT}`)
+            console.log(`Server is working on ${process.env.PORT}`);
         });
     } catch (err) {
         console.log(err);
     }
-}
+};
 
 start();
