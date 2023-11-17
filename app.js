02a083c59be53ae1676a54f744b3dfbf0e32805d
guinnod <99246260+guinnod@users.noreply.github.com>
2023-11-17 17:47:12 +0600

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 // Validation Middleware
 const authenticateUser = require('./middleware/authentication');
 
